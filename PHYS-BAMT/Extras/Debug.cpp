476da8ea91d2721b8615ab6f65582323c5caef0d
Connor Thompson <72395517+SparkMint@users.noreply.github.com>
2024-03-11 09:02:36 +0000

    Added debug stuff

---
+++ PHYS-BAMT/Extras/Debug.cpp
@@ -0,0 +1,72 @@
+﻿#define _CRT_SECURE_NO_WARNINGS // Disables the warnings for the use of unsafe functions. This is bad, but I dont care!
+#include "Debug.h"
+
+#include <ctime>
+#include <iostream>
+#include <sstream>
+#include <windows.h>
+
+namespace physBamt
+{
+    namespace Extra
+    {
+        int LOG_COLOUR = 15;
+        int WARN_COLOUR = 14;
+        int ERROR_COLOUR = 12;
+
+        void BamtLog(std::string output, BamtLogType type)
+        {
+            std::string typeString;
+            // Set the colour of our text to whatever severity the log is.
+            switch (type)
+            {
+            case BamtLogType::Info:
+                SetColour(LOG_COLOUR);
+                typeString = "INFO";
+                break;
+            case BamtLogType::Warning:
+                SetColour(WARN_COLOUR);
+                typeString = "WARNING";
+                break;
+            case BamtLogType::Error:
+                SetColour(ERROR_COLOUR);
+                typeString = "ERROR";
+                break;
+            }
+
+            // Print our log out to the console.
+            std::cout << "[" << GetTime() << "] " << typeString << " - " << output << '\n';
+        }
+
+        std::string GetTime()
+        {
+            std::ostringstream outputStream;
+
+            const time_t currentTime = time(0);
+            struct tm timeStruct;
+            char LogTime[80];
+            timeStruct = *localtime(&currentTime);
+            strftime(LogTime, sizeof(LogTime), "%Y-%m-%d %X", &timeStruct);
+
+            outputStream << LogTime;
+            return outputStream.str();
+        }
+
+        std::string GetPointerAddress(const void* pointer)
+        {
+            std::ostringstream address;
+            address << pointer;
+            return address.str();
+        }
+
+        void SetColour(const int colour)
+        {
+            const HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleTextAttribute(handle, colour);
+        }
+    }
+}
+
+
+
+
---
+++ PHYS-BAMT/Extras/Debug.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <string>
+
+namespace physBamt
+{
+    namespace Extra
+    {
+    enum class BamtLogType
+    {
+        Info,
+        Warning,
+        Error,
+    };
+
+    /// Logs a message to the console.
+    void BamtLog(std::string output = "", BamtLogType type = BamtLogType::Info);
+
+    /// Logs a message to the console, and also provides the address to an object in memory.
+    void BamtLog(BamtLogType type, std::string output, void* pointer);
+    
+    /// Returns the current time as a string. Returns the format [YEAR-MONTH-DAY HOUR:MINUTE:SECOND]
+    std::string GetTime();
+
+    /// Returns the address of a pointer as a string.
+    std::string GetPointerAddress(const void* pointer);
+
+    /// Changes the colour of the console text.
+    void SetColour(int colour);
+    }
+}
+
+
+
+
